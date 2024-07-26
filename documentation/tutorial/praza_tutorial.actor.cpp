41297abea114641e63d3cf635c78160e5c514756
Syed Paymaan Raza <1238752+spraza@users.noreply.github.com>
2024-07-26 15:02:25 -0700

    more

--- documentation/tutorial/praza_tutorial.actor.cpp
+++ documentation/tutorial/praza_tutorial.actor.cpp
@@ -15,140 +15,31 @@
 #include <iostream>
 #include "flow/actorcompiler.h"
 
-namespace {
-bool DEBUG = true;
-
-void print(const std::string& s) {
-	if (DEBUG) {
-		fmt::print("{}\n", s);
-	}
-}
-
-} // namespace
-
-ACTOR Future<double> io_num() {
-	print("io_num: start...");
-	wait(delay(1));
-	print("io_num: done...");
-	return 1;
-}
-
-ACTOR Future<Void> flow_timer() {
-	print("flow_timer: start...");
-	state double x = wait(io_num());
-	print("x: " + std::to_string(x));
-	state double start = g_network->now();
-	loop {
-		wait(delay(x));
-		print("elapsed time: " + std::to_string(g_network->now() - start));
-	}
-}
-
-ACTOR Future<Void> flow_future(Future<int> ready) {
-	int x = wait(ready);
-	print("x: " + std::to_string(x));
+ACTOR Future<Void> baz() {
+	fmt::print("baz_start\n");
+	wait(delay(5));
+	fmt::print("baz_end\n");
 	return Void();
-}
-
-ACTOR Future<Void> flow_promise() {
-	Promise<int> promise;
-	Future<Void> fut = flow_future(promise.getFuture());
-	wait(fut);
-	return Void();
-}
-
-ACTOR Future<Void> nested_delay() {
-	print("nested_delay: enter");
-	wait(delay(3));
-	print("nested_delay: exit");
-	return Void();
-}
-
-ACTOR Future<Void> my_delay() {
-	print("my_delay: enter");
-	wait(delay(3));
-	print("my_delay: mid1");
-	print("my_delay: mid2");
-	wait(nested_delay());
-	print("my_delay: exit");
-	return Void();
-}
-
-ACTOR Future<Void> test1() {
-	print("test1: enter");
-	state Future<Void> f = my_delay();
-	print("f is ready: " + std::to_string(f.isReady()));
-	wait(f);
-	print("f is ready: " + std::to_string(f.isReady()));
-	print("test1: exit");
-	return Void();
-}
-
-ACTOR void test2() {
-	print("test2: enter");
-	state Future<Void> f = my_delay();
-	print("f is ready: " + std::to_string(f.isReady()));
-	wait(f);
-	print("f is ready: " + std::to_string(f.isReady()));
-	print("test2: exit");
-	return;
 }
 
 ACTOR Future<Void> bar() {
-	// loop {
-	print("bar");
-	wait(delay(1));
-	// }
-	return Void();
-}
-
-ACTOR Future<Void> baz() {
-	// loop {
-	print("baz");
-	wait(delay(5));
-	// }
+	fmt::print("bar_start\n");
+	wait(baz());
+	fmt::print("bar_end\n");
 	return Void();
 }
 
 ACTOR Future<Void> foo() {
-	std::vector<Future<Void>> deps;
-	deps.reserve(2);
-	deps.emplace_back(bar());
-	deps.emplace_back(baz());
-	waitForAll(deps);
+	fmt::print("foo_start\n");
+	wait(bar());
+	fmt::print("foo_end\n");
 	return Void();
 }
 
-std::unordered_map<std::string, std::function<Future<Void>()>> actors = { { "timer", &flow_timer },
-	                                                                      { "promise", &flow_promise },
-	                                                                      { "test1", &test1 } };
-
 int main(int argc, char* argv[]) {
-	// Start up
 	platformInit();
 	g_network = newNet2(TLSConfig(), false, true);
-
-	// Decide which actor to run
-	std::function<Future<Void>()> toRun;
-
-	// Special cases
-	if (std::string{ argv[1] } == "test2") {
-		test2();
-		g_network->run();
-		return 0;
-	}
-	if (std::string{ argv[1] } == "foo") {
-		auto x = foo();
-		g_network->run();
-		return 0;
-	}
-
-	toRun = actors.at(argv[1]);
-
-	// Run and wait
-	auto f = stopAfter(toRun());
-
-	// Before exiting
+	auto x = stopAfter(foo());
 	g_network->run();
 	return 0;
 }
