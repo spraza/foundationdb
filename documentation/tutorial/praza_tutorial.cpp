7b79bb707997f493d412754ea49bcb32ab224d67
Syed Paymaan Raza <1238752+spraza@users.noreply.github.com>
2024-07-29 11:30:58 -0700

    cpp more

--- documentation/tutorial/praza_tutorial.cpp
+++ documentation/tutorial/praza_tutorial.cpp
@@ -1,13 +1,33 @@
+#include <cassert>
 #include <iostream>
+#include <sys/wait.h>
+#include <unistd.h>
 
 extern char** environ;
 
-int main() {
+void print_env_variables() {
 	char** env = environ;
-
 	while (*env) {
-		std::cout << *env << std::endl;
+		// std::cout << *env << std::endl;
 		env++;
+	}
+}
+
+int main() {
+	std::cout << "*** PARENT START, pid = " << getpid() << std::endl;
+	std::cout << "*** PARENT ENV VARIABLES" << std::endl;
+	print_env_variables();
+
+	pid_t pid = fork();
+	assert(pid != -1);
+	if (pid == 0) { // child
+		std::cout << "*** CHILD START, pid = " << getpid() << std::endl;
+		std::cout << "*** CHILD ENV VARIABLES" << std::endl;
+		print_env_variables();
+		std::cout << "*** CHILD END" << std::endl;
+	} else { // parent
+		waitpid(pid, nullptr, 0);
+		std::cout << "*** PARENT END" << std::endl;
 	}
 
 	return 0;
