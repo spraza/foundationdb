44f71f61b62990b84f258e12b95861f3779281ba
Syed Paymaan Raza <1238752+spraza@users.noreply.github.com>
2024-07-29 11:34:44 -0700

    cpp more

--- documentation/tutorial/praza_tutorial.cpp
+++ documentation/tutorial/praza_tutorial.cpp
@@ -8,13 +8,14 @@ extern char** environ;
 void print_env_variables() {
 	char** env = environ;
 	while (*env) {
-		// std::cout << *env << std::endl;
+		std::cout << *env << std::endl;
 		env++;
 	}
 }
 
 int main() {
 	std::cout << "*** PARENT START, pid = " << getpid() << std::endl;
+	setenv("PARENT_SPECIFIC_BEFORE_CHILD_CREATION", "parent_value", 1);
 	std::cout << "*** PARENT ENV VARIABLES" << std::endl;
 	print_env_variables();
 
@@ -22,10 +23,12 @@ int main() {
 	assert(pid != -1);
 	if (pid == 0) { // child
 		std::cout << "*** CHILD START, pid = " << getpid() << std::endl;
+		setenv("CHILD_SPECIFIC", "child_value", 1);
 		std::cout << "*** CHILD ENV VARIABLES" << std::endl;
 		print_env_variables();
 		std::cout << "*** CHILD END" << std::endl;
 	} else { // parent
+		setenv("PARENT_SPECIFIC_AFTER_CHILD_CREATION", "parent_value", 1);
 		waitpid(pid, nullptr, 0);
 		std::cout << "*** PARENT END" << std::endl;
 	}
