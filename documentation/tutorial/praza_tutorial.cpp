94b5b688af85824ae518b70d69b161537659bcb0
Syed Paymaan Raza <1238752+spraza@users.noreply.github.com>
2024-08-14 16:42:09 -0700

    more

--- documentation/tutorial/praza_tutorial.cpp
+++ documentation/tutorial/praza_tutorial.cpp
@@ -2,48 +2,14 @@
 #include <iostream>
 #include <sys/wait.h>
 #include <unistd.h>
-
-extern char** environ;
-
-void print_env_variables() {
-	char** env = environ;
-	while (*env) {
-		std::cout << *env << std::endl;
-		env++;
-	}
-}
-
-void foo() {
-	std::cout << "*** PARENT START, pid = " << getpid() << std::endl;
-	setenv("PARENT_SPECIFIC_BEFORE_CHILD_CREATION", "parent_value", 1);
-	std::cout << "*** PARENT ENV VARIABLES" << std::endl;
-	print_env_variables();
-
-	pid_t pid = fork();
-	assert(pid != -1);
-	if (pid == 0) { // child
-		std::cout << "*** CHILD START, pid = " << getpid() << std::endl;
-		setenv("CHILD_SPECIFIC", "child_value", 1);
-		std::cout << "*** CHILD ENV VARIABLES" << std::endl;
-		print_env_variables();
-		std::cout << "*** CHILD END" << std::endl;
-	} else { // parent
-		setenv("PARENT_SPECIFIC_AFTER_CHILD_CREATION", "parent_value", 1);
-		waitpid(pid, nullptr, 0);
-		std::cout << "*** PARENT END" << std::endl;
-	}
-}
-
-extern char** environ;
-
-void bar() {
-	for (char** env = environ; *env != nullptr; ++env) {
-		std::cout << *env << std::endl;
-	}
-}
+#include "flow/DeterministicRandom.h"
+#include "flow/Platform.h"
 
 int main() {
-	// foo();
-	bar();
+	auto seed = 132; // platform::getRandomSeed()
+	auto d = DeterministicRandom(seed);
+	for (int i = 0; i < 10; ++i) {
+		std::cout << d.randomUInt64() << std::endl;
+	}
 	return 0;
 }
