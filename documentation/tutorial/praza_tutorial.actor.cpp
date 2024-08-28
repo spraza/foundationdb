84c4a8b8fc968141cfe2d848630a1895417af5e5
Syed Paymaan Raza <1238752+spraza@users.noreply.github.com>
2024-08-27 23:48:02 -0700

    abt

--- documentation/tutorial/praza_tutorial.actor.cpp
+++ documentation/tutorial/praza_tutorial.actor.cpp
@@ -22,146 +22,15 @@
 
 #include "flow/actorcompiler.h"
 
-enum TutorialWellKnownEndpoints { WLTOKEN_KV_SERVER = WLTOKEN_FIRST_AVAILABLE, WLTOKEN_COUNT_IN_TUTORIAL };
-bool isServer = false;
-
-// Owned by server
-// Server can serialize and send it to client via getInterface RPC
-struct KVInterface {
-	constexpr static FileIdentifier file_identifier = 3152015;
-
-	RequestStream<struct ConnectRequest> connectRequest;
-	RequestStream<struct SetRequest> setRequest;
-	RequestStream<struct GetRequest> getRequest;
-
-	template <class Ar>
-	void serialize(Ar& ar) {
-		serializer(ar, connectRequest, setRequest, getRequest);
-	}
-};
-
-struct ConnectRequest {
-	constexpr static FileIdentifier file_identifier = 3152016;
-
-	ReplyPromise<KVInterface> reply;
-
-	template <class Ar>
-	void serialize(Ar& ar) {
-		serializer(ar, reply);
-	}
-};
-
-struct SetRequest {
-	constexpr static FileIdentifier file_identifier = 3152017;
-
-	std::string key;
-	std::string val;
-	ReplyPromise<Void> reply;
-
-	template <class Ar>
-	void serialize(Ar& ar) {
-		serializer(ar, key, val, reply);
-	}
-};
-
-struct GetRequest {
-	constexpr static FileIdentifier file_identifier = 3152018;
-
-	std::string key;
-	ReplyPromise<std::string> reply;
-
-	template <class Ar>
-	void serialize(Ar& ar) {
-		serializer(ar, key, reply);
-	}
-};
-
-ACTOR Future<Void> server() {
-	state KVInterface ifx;
-	state std::unordered_map<std::string, std::string> store;
-	ifx.connectRequest.makeWellKnownEndpoint(WLTOKEN_KV_SERVER, TaskPriority::DefaultEndpoint);
-	loop {
-		choose {
-			when(ConnectRequest req = waitNext(ifx.connectRequest.getFuture())) {
-				req.reply.send(ifx);
-			}
-			when(GetRequest req = waitNext(ifx.getRequest.getFuture())) {
-				auto iter = store.find(req.key);
-				if (iter == store.end()) {
-					req.reply.sendError(io_error());
-				} else {
-					req.reply.send(iter->second);
-				}
-			}
-			when(SetRequest req = waitNext(ifx.setRequest.getFuture())) {
-				store[req.key] = req.val;
-				req.reply.send(Void());
-			}
-		}
-	}
-}
-
-ACTOR Future<KVInterface> connect() {
-	NetworkAddress serverAddress = NetworkAddress::parse("127.0.0.1:6666");
-	auto reqStream =
-	    RequestStream<ConnectRequest>(Endpoint::wellKnown({ .address = serverAddress }, WLTOKEN_KV_SERVER));
-	KVInterface result_ifx = wait(reqStream.getReply(ConnectRequest()));
-	return result_ifx;
-}
-
-ACTOR Future<Void> client() {
-	state KVInterface ifx = wait(connect());
-	wait(ifx.setRequest.getReply(SetRequest{ .key = "foo", .val = "bar" }));
-	state std::string val = wait(ifx.getRequest.getReply(GetRequest{ .key = "foo" }));
-	std::cout << val << std::endl;
+ACTOR Future<Void> foo() {
+	wait(delay(1));
 	return Void();
-}
-
-ACTOR Future<Void> start() {
-	std::vector<Future<Void>> all;
-	if (isServer) {
-		all.push_back(server());
-	} else {
-		all.push_back(client());
-	}
-	wait(waitForAll(all));
-	return Void();
-}
-
-void setup_flow_server_rpc() {
-	const std::string port{ "6666" };
-	FlowTransport::createInstance(false, 0, WLTOKEN_COUNT_IN_TUTORIAL);
-	NetworkAddress publicAddress = NetworkAddress::parse("0.0.0.0:" + port);
-	try {
-		auto listenError = FlowTransport::transport().bind(publicAddress, publicAddress);
-		if (listenError.isError()) {
-			listenError.get();
-		}
-	} catch (Error& e) {
-		std::cout << format("Error while binding to address (%d): %s\n", e.code(), e.what());
-	}
-}
-
-void setup_flow_client_rpc() {
-	FlowTransport::createInstance(true, 0, WLTOKEN_COUNT_IN_TUTORIAL);
 }
 
 int main(int argc, char* argv[]) {
-	for (int i = 1; i < argc; ++i) {
-		std::string arg(argv[i]);
-		if (arg == "--is-server") {
-			isServer = true;
-		}
-	}
-	std::cout << "isServer = " << isServer << std::endl;
 	platformInit();
 	g_network = newNet2(TLSConfig(), false, true);
-	if (isServer) {
-		setup_flow_server_rpc();
-	} else {
-		setup_flow_client_rpc();
-	}
-	auto x = stopAfter(start());
+	auto x = stopAfter(foo());
 	g_network->run();
 	return 0;
 }
