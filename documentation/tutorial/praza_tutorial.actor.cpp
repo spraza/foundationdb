0bc0f6dc6bbcacb573f7e3038c7339bcb5ae07cd
Syed Paymaan Raza <1238752+spraza@users.noreply.github.com>
2024-09-10 12:48:26 -0700

    loop/choose/when pattern

--- documentation/tutorial/praza_tutorial.actor.cpp
+++ documentation/tutorial/praza_tutorial.actor.cpp
@@ -22,8 +22,50 @@
 
 #include "flow/actorcompiler.h"
 
-ACTOR Future<Void> foo() {
+struct State {
+	State() {
+		std::cout << "State ctor default"
+		          << "\n";
+	}
+	State(std::string type) : type(type) { std::cout << "State ctor explicit, type: " << type << "\n"; }
+	std::string type;
+	~State() { std::cout << "State dtor, type: " << type << "\n"; }
+	void p() { std::cout << "State print, type: " << type << "\n"; }
+};
+
+ACTOR Future<Void> baz() {
+	std::cout << "baz enter\n";
+	state State s("baz");
 	wait(delay(1));
+	s.p();
+	std::cout << "baz exit\n";
+	return Void();
+}
+
+ACTOR Future<Void> bar() {
+	std::cout << "bar enter\n";
+	state State s("bar");
+	wait(delay(2));
+	s.p();
+	std::cout << "bar exit\n";
+	return Void();
+}
+
+ACTOR Future<Void> foo() {
+	state Future<Void> x = bar();
+	state Future<Void> y = baz();
+	loop choose {
+		when(wait(x)) {
+			std::cout << "foo1\n";
+			// y.cancel();
+			break;
+		}
+		when(wait(y)) {
+			std::cout << "foo2\n";
+			// x.cancel();
+			break;
+		}
+	}
 	return Void();
 }
 
