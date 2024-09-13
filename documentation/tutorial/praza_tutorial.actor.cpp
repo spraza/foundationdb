cb5aeaf1bc02f9fd13a53441b9038ddcf87b4987
Syed Paymaan Raza <1238752+spraza@users.noreply.github.com>
2024-09-12 17:09:53 -0700

    reference async var + mystery

--- documentation/tutorial/praza_tutorial.actor.cpp
+++ documentation/tutorial/praza_tutorial.actor.cpp
@@ -1,6 +1,8 @@
 #include "fdbrpc/FlowTransport.h"
 #include "fdbrpc/fdbrpc.h"
 #include "flow/Buggify.h"
+#include "flow/Error.h"
+#include "flow/FastRef.h"
 #include "flow/NetworkAddress.h"
 #include "flow/TaskPriority.h"
 #include "flow/genericactors.actor.h"
@@ -22,51 +24,27 @@
 
 #include "flow/actorcompiler.h"
 
-struct State {
-	State() {
-		std::cout << "State ctor default"
-		          << "\n";
+ACTOR Future<Void> bar(Reference<AsyncVar<int>> av) {
+	loop {
+		std::cout << "av.get() = " << av->get() << std::endl;
+		wait(av->onChange()); // notificiation
+		// wait(delay(1)); // poll every 1 second
 	}
-	State(std::string type) : type(type) { std::cout << "State ctor explicit, type: " << type << "\n"; }
-	std::string type;
-	~State() { std::cout << "State dtor, type: " << type << "\n"; }
-	void p() { std::cout << "State print, type: " << type << "\n"; }
-};
-
-ACTOR Future<Void> baz() {
-	std::cout << "baz enter\n";
-	state State s("baz");
-	wait(delay(1));
-	s.p();
-	std::cout << "baz exit\n";
-	return Void();
-}
-
-ACTOR Future<Void> bar() {
-	std::cout << "bar enter\n";
-	state State s("bar");
-	wait(delay(2));
-	s.p();
-	std::cout << "bar exit\n";
-	return Void();
 }
 
 ACTOR Future<Void> foo() {
-	state Future<Void> x = bar();
-	state Future<Void> y = baz();
-	loop choose {
-		when(wait(x)) {
-			std::cout << "foo1\n";
-			// y.cancel();
-			break;
-		}
-		when(wait(y)) {
-			std::cout << "foo2\n";
-			// x.cancel();
-			break;
+	state Reference<AsyncVar<int>> av = makeReference<AsyncVar<int>>(5);
+	loop {
+		choose {
+			when(wait(bar(av))) {}
+			// when(wait(delay(5))) {
+			// 	av->set(av->get() + 5);
+			// }
+			when(wait(delay(2))) {
+				// av->trigger();
+			}
 		}
 	}
-	return Void();
 }
 
 int main(int argc, char* argv[]) {
