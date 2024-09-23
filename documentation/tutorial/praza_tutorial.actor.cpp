9d1f8e6c0b080e82a23dfd7f503188244f8c8cd1
Syed Paymaan Raza <1238752+spraza@users.noreply.github.com>
2024-09-23 11:40:55 -0700

    actor error handling and cancelation learning notes

--- documentation/tutorial/praza_tutorial.actor.cpp
+++ documentation/tutorial/praza_tutorial.actor.cpp
@@ -16,7 +16,10 @@
 #include <cstdint>
 #include <functional>
 #include <new>
+#include <stdexcept>
 #include <string>
+#include <thread>
+// #include <chrono>
 #include <unordered_map>
 #include <memory>
 #include <vector>
@@ -24,32 +27,68 @@
 
 #include "flow/actorcompiler.h"
 
-int x{ 0 };
+// Learnings:
+//    1. throw is understood by actor compilers, and it expects flow Error type, not type std errors/exceptions
+//    2. depending on the error thrown, the actor compiler decides to crash the program or not (in which case, <no_op?>,
+//    but can be caught explicitly if you want)
+//    3. from actor readme.md:
+//          By default it is a compiler error to discard the result of a cancellable actor. If you don't think this is
+//          appropriate for your actor you can use the `[[flow_allow_discard]]` attribute. This does not apply to
+//          UNCANCELLABLE actors.
+//          related: also say "[[nodiscard]]" in the code. someday if needed, i can spend the time to understand this
+//          better
+//    4. how to cancel as a "client" of actor:
+//        (a) actor compiler cancels automatically based on scope of actor or any of
+//            its parents, where parents are defined as any actor instance that calls into the self actor instance - the
+//            parent can decide to wait on the current/self actor, in which case the current/self actor will be
+//            completed/ready first.
+//        (b) the future you get back from actor, just do f.cancel(). This is probably what actor compiler does anyway.
+//    5. on actor side, what happens in the event of cancelation:
+//         if you have a try-catch block in the actor, and the parent cancels it (because of timeout/scope or not
+//         waiting) if you don't have a try-catch, then nothing happens and unless blocked, the actor will exit if the
+//         actor is blocked on some synchoronous code, that will have to finish first before cancellation exits another
+//         way to think about it: cancelation would happen before the next "wait"
+//    6. if you don't want on an actor, its errors (if any) are not propagated to you as the parent
+//    7. is cancellation cooperative? e.g. if client cancels, does actor have a choice?
+//            ans: actor does not have a choice, it always gets the error exception, so unless there is a weird way
+//            where actor catches exception and then somehow block or recursively call itself, mostly the actor will
+//            have to exit out. In fact, if you don't explicitly have code for error handling in actor, then basically
+//            you'll exit out anyway.
+//    8. how to make actor uncancellable? ACTOR UNCANCELLBALE keyword, or return void instead of Future<Void>
+//         what happens in these cases is weird: actor does not get the error but still exits.. someday if needed i can
+//         spend the time to understand this better
 
-ACTOR Future<Void> measure() {
-	wait(delay(0.1));
-	std::cout << x << std::endl;
-	return Void();
-}
-
-ACTOR Future<Void> increase() {
-	wait(delay(5));
-	x += 1;
-	wait(Never());
-	UNREACHABLE();
+ACTOR UNCANCELLABLE Future<Void> bar(int x) {
+	//	using namespace std::chrono_literals;
+	try {
+		// std::this_thread::sleep_for(10s); // will delay cancelation
+		wait(delay(10));
+		if (x > 0) {
+			// throw not_implemented();
+			throw internal_error();
+		}
+	} catch (Error& e) {
+		std::cout << "bar1\n";
+		std::cout << e.name() << ", " << e.code() << ", " << e.what() << std::endl;
+	}
 	return Void();
 }
 
 ACTOR Future<Void> foo() {
-	state Future<Void> f = increase();
-	loop choose {
-		when(wait(delay(3))) {
-			wait(measure());
-		}
-		when(wait(f)) {
-			std::cout << "bar\n";
-		}
+	try {
+		wait(delay(3));
+		// wait(bar(1));
+		state Future<Void> f = bar(1);
+		std::cout << "waiting...\n";
+		wait(delay(2));
+		f.cancel();
+		// wait(f);
+	} catch (Error& e) {
+		std::cout << "foo1\n";
+		std::cout << e.name() << ", " << e.code() << ", " << e.what() << std::endl;
+		return Void();
 	}
+	return Void();
 }
 
 int main(int argc, char* argv[]) {
