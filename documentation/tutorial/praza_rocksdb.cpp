be5285269deef0109da9386a53edfdf54c5ff06c
Syed Paymaan Raza <1238752+spraza@users.noreply.github.com>
2024-12-05 14:31:53 -0800

    more rocksdb code

--- documentation/tutorial/praza_rocksdb.cpp
+++ documentation/tutorial/praza_rocksdb.cpp
@@ -9,38 +9,69 @@ int main() {
 	const std::string path{ "/tmp/testdb" };
 	rocksdb::Options options;
 	options.create_if_missing = true;
-	rocksdb::ColumnFamilyHandle* cf;
-	std::vector<rocksdb::ColumnFamilyHandle*> handles;
+	options.error_if_exists = false;
+
+	// Step 1: Open the database and create a column family
+	rocksdb::ColumnFamilyHandle* cf = nullptr;
 	{
-		auto status = rocksdb::DB::Open(options, path, &db);
+		rocksdb::Status status = rocksdb::DB::Open(options, path, &db);
 		assert(status.ok());
-		auto status2 = db->CreateColumnFamily(rocksdb::ColumnFamilyOptions(), "MyRocksDBCheckpoint", &cf);
-		handles.push_back(cf);
-		assert(status2.ok());
-		assert(db->Put(rocksdb::WriteOptions(), cf, "foo", "bar").ok());
-		assert(db->DestroyColumnFamilyHandle(cf).ok());
-		assert(db->Close().ok());
-		// delete db;
+		status = db->CreateColumnFamily(rocksdb::ColumnFamilyOptions(), "RocksDBCheckpoint", &cf);
+		assert(status.ok());
+		status = db->Put(rocksdb::WriteOptions(), cf, "foo", "bar");
+		assert(status.ok());
+
+		// Do not drop the column family here
+		db->DestroyColumnFamilyHandle(cf);
+		db->Close();
+		delete db;
 	}
+
+	// Step 2: List column families
 	std::vector<rocksdb::ColumnFamilyDescriptor> cfDescriptors;
 	{
 		std::vector<std::string> columnFamilies;
-		assert(rocksdb::DB::ListColumnFamilies(options, path, &columnFamilies).ok());
+		rocksdb::Status status = rocksdb::DB::ListColumnFamilies(options, path, &columnFamilies);
+		assert(status.ok());
 		for (const std::string& name : columnFamilies) {
 			cfDescriptors.emplace_back(name, rocksdb::ColumnFamilyOptions());
 		}
 	}
+
+	// Step 3: Open the database for read-only access and read the key
 	{
-		auto status = rocksdb::DB::OpenForReadOnly(options, path, cfDescriptors, &handles, &db);
+		std::vector<rocksdb::ColumnFamilyHandle*> handles;
+		rocksdb::Status status = rocksdb::DB::OpenForReadOnly(options, path, cfDescriptors, &handles, &db);
 		assert(status.ok());
-		// std::string val;
-		//  assert(db->Get(rocksdb::ReadOptions(), "foo", &val).ok());
-		//  std::cout << fmt::format("get({}) = {}", "foo", val);
-		for (auto handle : handles) {
-			assert(db->DestroyColumnFamilyHandle(handle).ok());
+
+		// Find the handle for "RocksDBCheckpoint"
+		rocksdb::ColumnFamilyHandle* targetHandle = nullptr;
+		for (size_t i = 0; i < cfDescriptors.size(); ++i) {
+			if (cfDescriptors[i].name == "RocksDBCheckpoint") {
+				targetHandle = handles[i];
+				break;
+			}
 		}
-		assert(db->Close().ok());
+
+		if (targetHandle) {
+			std::string val;
+			rocksdb::Status getStatus = db->Get(rocksdb::ReadOptions(), targetHandle, "foo", &val);
+			if (getStatus.ok()) {
+				std::cout << fmt::format("get({}) = {}", "foo", val) << std::endl;
+			} else {
+				std::cout << "Key not found\n";
+			}
+		} else {
+			std::cout << "Column family 'RocksDBCheckpoint' not found\n";
+		}
+
+		// Clean up handles
+		for (auto handle : handles) {
+			db->DestroyColumnFamilyHandle(handle);
+		}
+		db->Close();
+		delete db;
 	}
-	// std::cout << fmt::format("my str is {}", "blah") << std::endl;
+
 	return 0;
 }
