17dd18952e4a7ba87027a6ff612df1d490b943d3
Syed Paymaan Raza <1238752+spraza@users.noreply.github.com>
2025-02-04 15:29:27 -0800

    fix

--- fdbserver/workloads/ConfigureDatabase.actor.cpp
+++ fdbserver/workloads/ConfigureDatabase.actor.cpp
@@ -322,7 +322,25 @@ struct ConfigureDatabaseWorkload : TestWorkload {
 				state bool pass = true;
 				state std::vector<StorageServerInterface> storageServers = wait(getStorageServers(cx));
 
+				state std::unordered_map<std::string /* dc id */, int /* number of ss in that dc id */> dcIdToSSCount;
+				for (const auto& ss : storageServers) {
+					if (ss.locality.dcId().present()) {
+						const auto& dcId = ss.locality.dcId().get().toString();
+						if (!dcIdToSSCount.contains(dcId)) {
+							dcIdToSSCount[dcId] = 0;
+						}
+						dcIdToSSCount[dcId] += 1;
+					}
+				}
+
 				for (i = 0; i < storageServers.size(); i++) {
+					if (!storageServers[i].locality.dcId().present()) {
+						continue;
+					}
+					if (dcIdToSSCount[storageServers[i].locality.dcId().get().toString()] <= 3) {
+						continue;
+					}
+
 					// Check that each storage server has the correct key value store type
 					if (!storageServers[i].isTss() &&
 					    (wiggleLocalityKeyValue == "0" ||
