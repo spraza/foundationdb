4d57475a1b74b56554a855ab37cc27ba33179163
Syed Paymaan Raza <1238752+spraza@users.noreply.github.com>
2025-02-04 17:58:47 -0800

    fix

--- fdbserver/workloads/ConfigureDatabase.actor.cpp
+++ fdbserver/workloads/ConfigureDatabase.actor.cpp
@@ -307,13 +307,39 @@ struct ConfigureDatabaseWorkload : TestWorkload {
 		// perpetual wiggle will be forced to close For other cases, later ConsistencyCheck will check KV store type
 		// there
 		if (self->allowTestStorageMigration || self->waitStoreTypeCheck) {
+
+			state DatabaseConfiguration dbConf = wait(getDatabaseConfiguration(cx));
+			state std::vector<StorageServerInterface> allStorageServers = wait(getStorageServers(cx));
+
+			state std::unordered_map<std::string /* dc id */, int /* number of ss in that dc id */> dcIdToSSCount;
+			for (const auto& ss : allStorageServers) {
+				if (ss.locality.dcId().present()) {
+					const auto& dcId = ss.locality.dcId().get().toString();
+					if (!dcIdToSSCount.contains(dcId)) {
+						dcIdToSSCount[dcId] = 0;
+					}
+					dcIdToSSCount[dcId] += 1;
+				}
+			}
+
+			state int idx;
+			for (idx = 0; idx < allStorageServers.size(); idx++) {
+				// if (!storageServers[i].locality.dcId().present()) {
+				// 		continue;
+				// }
+				if (dcIdToSSCount[allStorageServers[idx].locality.dcId().get().toString()] <= dbConf.storageTeamSize) {
+					wait(success(IssueConfigurationChange(
+					    cx, "perpetual_storage_wiggle=1 storage_migration_type=aggressive", false)));
+					break;
+				}
+			}
+
 			loop {
 				// There exists a race where the check can start before the last transaction that singleDB issued
 				// finishes, if singleDB gets actor cancelled from a timeout at the end of a test. This means the
 				// configuration needs to be re-read in case it changed since the last loop, since it could
 				// read a stale storage engine type from the configuration initially.
 				state DatabaseConfiguration conf = wait(getDatabaseConfiguration(cx));
-				state int ssReplicationFactor = conf.storageTeamSize;
 
 				state std::string wiggleLocalityKeyValue = conf.perpetualStorageWiggleLocality;
 				state std::vector<std::pair<Optional<Value>, Optional<Value>>> wiggleLocalityKeyValues =
@@ -323,25 +349,7 @@ struct ConfigureDatabaseWorkload : TestWorkload {
 				state bool pass = true;
 				state std::vector<StorageServerInterface> storageServers = wait(getStorageServers(cx));
 
-				state std::unordered_map<std::string /* dc id */, int /* number of ss in that dc id */> dcIdToSSCount;
-				for (const auto& ss : storageServers) {
-					if (ss.locality.dcId().present()) {
-						const auto& dcId = ss.locality.dcId().get().toString();
-						if (!dcIdToSSCount.contains(dcId)) {
-							dcIdToSSCount[dcId] = 0;
-						}
-						dcIdToSSCount[dcId] += 1;
-					}
-				}
-
 				for (i = 0; i < storageServers.size(); i++) {
-					if (!storageServers[i].locality.dcId().present()) {
-						continue;
-					}
-					if (dcIdToSSCount[storageServers[i].locality.dcId().get().toString()] <= ssReplicationFactor) {
-						continue;
-					}
-
 					// Check that each storage server has the correct key value store type
 					if (!storageServers[i].isTss() &&
 					    (wiggleLocalityKeyValue == "0" ||
