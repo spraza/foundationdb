a82c038ee9489d9899a9993ff1ae2c6cdad619f5
Syed Paymaan Raza <1238752+spraza@users.noreply.github.com>
2025-02-25 17:47:31 -0800

    attempted fix

--- fdbserver/include/fdbserver/LogSystem.h
+++ fdbserver/include/fdbserver/LogSystem.h
@@ -292,7 +292,8 @@ struct ILogSystem {
 		                 bool parallelGetMore,
 		                 std::vector<LocalityData> const& tLogLocalities,
 		                 Reference<IReplicationPolicy> const tLogPolicy,
-		                 int tLogReplicationFactor);
+		                 int tLogReplicationFactor,
+		                 const std::vector<int>& pushLocations = {});
 		MergedPeekCursor(std::vector<Reference<IPeekCursor>> const& serverCursors,
 		                 LogMessageVersion const& messageVersion,
 		                 int bestServer,
