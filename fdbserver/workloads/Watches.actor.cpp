0b5e168132c410bbafb63cb6d9a503d5ac012a30
Syed Paymaan Raza <1238752+spraza@users.noreply.github.com>
2025-03-19 22:23:59 -0700

    more logging

--- fdbserver/workloads/Watches.actor.cpp
+++ fdbserver/workloads/Watches.actor.cpp
@@ -18,6 +18,7 @@
  * limitations under the License.
  */
 
+#include "fdbclient/FDBTypes.h"
 #include "fdbrpc/DDSketch.h"
 #include "fdbclient/NativeAPI.actor.h"
 #include "fdbserver/TesterInterface.actor.h"
@@ -144,18 +145,42 @@ struct WatchesWorkload : TestWorkload {
 							tr->set(setKey, watchValue.get());
 						else
 							tr->clear(setKey);
-						//TraceEvent("WatcherSetStart").detail("Watch", printable(watchKey)).detail("Set", printable(setKey)).detail("Value", printable( watchValue ) );
+						TraceEvent("WatcherSetStart")
+						    .detail("WatchKey", printable(watchKey))
+						    .detail("WatchValue", printable(watchValue))
+						    .detail("SetKey", printable(setKey))
+						    .detail("SetValue", printable(setValue));
 						wait(tr->commit());
-						//TraceEvent("WatcherSetFinish").detail("Watch", printable(watchKey)).detail("Set", printable(setKey)).detail("Value", printable( watchValue ) ).detail("Ver", tr->getCommittedVersion());
+						TraceEvent("WatcherSetFinish")
+						    .detail("WatchKey", printable(watchKey))
+						    .detail("WatchValue", printable(watchValue))
+						    .detail("SetKey", printable(setKey))
+						    // .detail("SetValue", printable(setValue))
+						    .detail("Ver", tr->getCommittedVersion());
 					} else {
-						//TraceEvent("WatcherWatch").detail("Watch", printable(watchKey));
+						TraceEvent("WatcherWatchStart")
+						    .detail("WatchKey", printable(watchKey))
+						    .detail("WatchValue", printable(watchValue))
+						    .detail("SetKey", printable(setKey))
+						    .detail("SetValue", printable(setValue));
+						;
 						state Future<Void> watchFuture = tr->watch(makeReference<Watch>(watchKey, watchValue));
 						wait(tr->commit());
+						TraceEvent("WatcherWatchCommit")
+						    .detail("WatchKey", printable(watchKey))
+						    .detail("WatchValue", printable(watchValue))
+						    .detail("SetKey", printable(setKey));
+						// .detail("SetValue", printable(setValue));
 						if (BUGGIFY) {
 							// Make watch future outlive transaction
 							tr.reset();
 						}
 						wait(watchFuture);
+						TraceEvent("WatcherWatchFinish")
+						    .detail("WatchKey", printable(watchKey))
+						    .detail("WatchValue", printable(watchValue))
+						    .detail("SetKey", printable(setKey));
+						// .detail("SetValue", printable(setValue));
 						if (watchValue.present())
 							lastValue = watchValue;
 					}
@@ -204,7 +229,9 @@ struct WatchesWorkload : TestWorkload {
 					tr->clear(startKey);
 
 				co_await tr->commit();
-				TraceEvent("WatchesWorker5");
+				TraceEvent("WatchesWorker5")
+				    .detail("StartKey", printable(startKey))
+				    .detail("ExpectedValue", printable(expectedValue));
 				CODE_PROBE(expectedValue.present(), "watches workload set a key");
 				CODE_PROBE(!expectedValue.present(), "watches workload clear a key");
 				co_return;
