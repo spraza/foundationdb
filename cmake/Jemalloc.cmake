e397f873544d5c5560bd1e70932cee7ecbf65f17
Syed Paymaan Raza <1238752+spraza@users.noreply.github.com>
2025-07-11 15:57:56 -0700

    disable jemalloc sampling

--- cmake/Jemalloc.cmake
+++ cmake/Jemalloc.cmake
@@ -15,7 +15,7 @@ ExternalProject_add(Jemalloc_project
   BUILD_BYPRODUCTS "${JEMALLOC_DIR}/include/jemalloc/jemalloc.h"
   "${JEMALLOC_DIR}/lib/libjemalloc.a"
   "${JEMALLOC_DIR}/lib/libjemalloc_pic.a"  
-  CONFIGURE_COMMAND CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER} ./configure --prefix=${JEMALLOC_DIR} --enable-static --disable-cxx --enable-prof --enable-stats --with-pic --with-malloc-conf=prof:true,prof_prefix:/var/tmp/fdbserver
+  CONFIGURE_COMMAND CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER} ./configure --prefix=${JEMALLOC_DIR} --enable-static --disable-cxx --enable-prof --enable-stats --with-pic --with-malloc-conf=prof:true,lg_prof_sample:0,prof_active:true,prof_final:true,prof_prefix:/var/tmp/fdbserver
   BUILD_IN_SOURCE ON
   BUILD_COMMAND make
   INSTALL_DIR "${JEMALLOC_DIR}"
