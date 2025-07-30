e656cffcecf836470cd56ac66ae195a159a43130
Syed Paymaan Raza <1238752+spraza@users.noreply.github.com>
2025-07-30 13:25:24 -0700

    arena observability v8 - only fastalloc96 + basic counters + fix bug

--- flow/actorcompiler/ActorCompiler.cs
+++ flow/actorcompiler/ActorCompiler.cs
@@ -534,6 +534,10 @@ namespace actorcompiler
             if (generateProbes) {
                 fun.WriteLine("fdb_probe_actor_enter(\"{0}\", {1}, {2});", name, thisAddress, index);
             }
+            //fun.WriteLine("std::cout << \"entered actor {0}\" << std::endl;", name);
+            var fileName = Path.GetFileName(sourceFile);          // "SimKmsConnector.actor.cpp"
+            //fun.WriteLine($"ArenaStatTypes::getActorStack().push_back(\"{fileName}:{name}\");"); // no :line, just file + actor name
+            fun.WriteLine($"ArenaStatTypes::getActorStack().push_back(\"{name}\");"); // no :line, just file + actor name
             var blockIdentifier = GetUidFromString(fun.name);
             fun.WriteLine("#ifdef WITH_ACAC");
             fun.WriteLine("static constexpr ActorBlockIdentifier __identifier = UID({0}UL, {1}UL);", blockIdentifier.Item1, blockIdentifier.Item2);
@@ -545,6 +549,7 @@ namespace actorcompiler
             if (generateProbes) {
                 fun.WriteLine("fdb_probe_actor_exit(\"{0}\", {1}, {2});", name, thisAddress, index);
             }
+            fun.WriteLine($"ArenaStatTypes::getActorStack().pop_back();");
         }
 
         void ProbeCreate(Function fun, string name) {
