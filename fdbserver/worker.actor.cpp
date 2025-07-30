be0607f1f896690ce6ce99d2edecc4c24adbfc35
Syed Paymaan Raza <1238752+spraza@users.noreply.github.com>
2025-07-30 13:25:24 -0700

    arena observability v4

--- fdbserver/worker.actor.cpp
+++ fdbserver/worker.actor.cpp
@@ -4260,6 +4260,17 @@ Optional<UID> readClusterId(std::string filePath) {
 	return clusterId;
 }
 
+ACTOR Future<Void> memoryProfiler() {
+	loop {
+		TraceEvent("NewMemoryProfiler")
+		    .detail("ArenasCreated", ArenaStatTypes::getArenasCreated())
+		    .detail("ArenasDestroyed", ArenaStatTypes::getArenasDestroyed())
+		    .detail("ArenasActive", ArenaStatTypes::getArenasActive())
+		    .detail("TopActorsAlloc", ArenaStatTypes::getActorMap().topN(5));
+		wait(delay(15));
+	}
+}
+
 ACTOR Future<Void> fdbd(Reference<IClusterConnectionRecord> connRecord,
                         LocalityData localities,
                         ProcessClass processClass,
@@ -4290,6 +4301,7 @@ ACTOR Future<Void> fdbd(Reference<IClusterConnectionRecord> connRecord,
 
 	actors.push_back(serveProtocolInfo());
 	actors.push_back(serveProcess());
+	actors.push_back(memoryProfiler());
 
 	try {
 		ServerCoordinators coordinators(connRecord, configDBType);
