bc3aa9a758baafdf9dcab9417f0d01c5c1e3b23b
Syed Paymaan Raza <1238752+spraza@users.noreply.github.com>
2025-08-04 11:07:15 -0700

    actor compiler: file:actor_name -> actor_name

--- flow/actorcompiler/ActorCompiler.cs
+++ flow/actorcompiler/ActorCompiler.cs
@@ -536,7 +536,8 @@ namespace actorcompiler
             }
             //fun.WriteLine("std::cout << \"entered actor {0}\" << std::endl;", name);
             var fileName = Path.GetFileName(sourceFile);          // "SimKmsConnector.actor.cpp"
-            fun.WriteLine($"ArenaStatTypes::getActorStack().push_back(\"{fileName}:{name}\");"); // no :line, just file + actor name
+            //fun.WriteLine($"ArenaStatTypes::getActorStack().push_back(\"{fileName}:{name}\");"); // no :line, just file + actor name
+            fun.WriteLine($"ArenaStatTypes::getActorStack().push_back(\"{name}\");"); // no :line, just file + actor name
             var blockIdentifier = GetUidFromString(fun.name);
             fun.WriteLine("#ifdef WITH_ACAC");
             fun.WriteLine("static constexpr ActorBlockIdentifier __identifier = UID({0}UL, {1}UL);", blockIdentifier.Item1, blockIdentifier.Item2);
