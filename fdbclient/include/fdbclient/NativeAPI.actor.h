1041805c1a649c153dee86b4ef0770dccfc2aa3f
Syed Paymaan Raza <1238752+spraza@users.noreply.github.com>
2025-08-07 12:51:28 -0700

    Revert "Merge pull request #11600 from jzhou77/coroutine"
    This reverts commit 9a914d940063f2e803c308d6d145a389a29d2acd, reversing
    changes made to bf292a6c9535349ea2b6f806439196ec6e47394a.

--- fdbclient/include/fdbclient/NativeAPI.actor.h
+++ fdbclient/include/fdbclient/NativeAPI.actor.h
@@ -119,9 +119,6 @@ public:
 
 	const UniqueOrderedOptionList<FDBTransactionOptions>& getTransactionDefaults() const;
 
-	template <std::invocable<Transaction*> Fun>
-	Future<Void> run(Fun fun);
-
 private:
 	Reference<DatabaseContext> db;
 };
@@ -580,24 +577,6 @@ private:
 	Promise<Void> commitResult;
 	Future<Void> committing;
 };
-
-template <std::invocable<Transaction*> Fun>
-Future<Void> Database::run(Fun fun) {
-	Transaction tr(*this);
-	Future<Void> onError;
-	while (true) {
-		if (onError.isValid()) {
-			co_await onError;
-			onError = Future<Void>();
-		}
-		try {
-			co_await fun(&tr);
-			co_return;
-		} catch (Error& e) {
-			onError = tr.onError(e);
-		}
-	}
-}
 
 ACTOR Future<Version> waitForCommittedVersion(Database cx, Version version, SpanContext spanContext);
 ACTOR Future<Standalone<VectorRef<DDMetricsRef>>> waitDataDistributionMetricsList(Database cx,
