c16c3fcd51d40faefec246113d335e56531a997b
Syed Paymaan Raza <1238752+spraza@users.noreply.github.com>
2026-01-08 11:26:02 -0800

    test v1, failover only, jsd 10k specific: 20260108-210940-praza-r145770855-f4f7fe66c2-0eb52bcd3806bf35

--- fdbserver/workloads/StuckFailback.actor.cpp
+++ fdbserver/workloads/StuckFailback.actor.cpp
@@ -18,19 +18,85 @@
  * limitations under the License.
  */
 
+#include "fdbclient/GenericManagementAPI.actor.h"
+#include "fdbclient/ManagementAPI.actor.h"
+#include "fdbclient/NativeAPI.actor.h"
+#include "fdbserver/RecoveryState.h"
+#include "fdbserver/ServerDBInfo.actor.h"
 #include "fdbserver/workloads/workloads.actor.h"
+
+#include "flow/Trace.h"
 #include "flow/actorcompiler.h" // This must be the last #include.
 
 struct StuckFailbackWorkload : TestWorkload {
 	static constexpr auto NAME = "StuckFailback";
+	bool enabled;
+	double testDuration;
+	bool testSuccess;
 
-	StuckFailbackWorkload(WorkloadContext const& wcx) : TestWorkload(wcx) {}
+	StuckFailbackWorkload(WorkloadContext const& wcx) : TestWorkload(wcx) {
+		enabled =
+		    !clientId && g_network->isSimulated(); // only do this on the "first" client, and only when in simulation
+		testDuration = getOption(options, "testDuration"_sr, 300.0);
+		testSuccess = false;
+		g_simulator->usableRegions = 2; // is this needed? similar code in toml file
+	}
 
 	Future<Void> setup(Database const& cx) override { return Void(); }
 
-	Future<Void> start(Database const& cx) override { return Void(); }
+	Future<Void> start(Database const& cx) override {
+		if (!enabled) {
+			return Void();
+		}
+		return timeout(reportErrors(client(this, cx), "StuckFailbackTimedOut"), testDuration, Void());
+	}
 
-	Future<bool> check(Database const& cx) override { return true; }
+	Future<bool> check(Database const& cx) override {
+		if (!enabled) {
+			return true;
+		}
+		if (!testSuccess) {
+			TraceEvent(SevError, "StuckFailbackWorkloadFailed");
+		}
+		return testSuccess;
+	}
+
+	ACTOR static Future<Void> doFailover(StuckFailbackWorkload* self, Database cx) {
+		TraceEvent("StuckFailbackWorkload_FailoverVerify");
+
+		wait(waitForPrimaryDC(cx, "0"_sr));
+
+		TraceEvent("StuckFailbackWorkload_FailoverBegin");
+
+		wait(success(ManagementAPI::changeConfig(cx.getReference(), g_simulator->disablePrimary, true)));
+
+		TraceEvent("StuckFailbackWorkload_FailoverWait");
+
+		wait(waitForPrimaryDC(cx, "1"_sr));
+
+		TraceEvent("StuckFailbackWorkload_FailoverComplete");
+
+		return Void();
+	}
+
+	ACTOR Future<Void> client(StuckFailbackWorkload* self, Database cx) {
+		TraceEvent("StuckFailbackWorkload_ClientBegin");
+		while (self->dbInfo->get().recoveryState < RecoveryState::FULLY_RECOVERED) {
+			TraceEvent("StuckFailbackWorkload_ClientNotFullyRecovered");
+			wait(self->dbInfo->onChange());
+		}
+		TraceEvent("StuckFailbackWorkload_ClientFullyRecovered");
+
+		wait(self->doFailover(self, cx));
+
+		// todo: failback
+
+		TraceEvent("StuckFailbackWorkload_ClientEnd");
+
+		self->testSuccess = true;
+
+		return Void();
+	}
 
 	void getMetrics(std::vector<PerfMetric>& m) override {}
 };
